b0aab0b6600c1863cf7a6e701bb31595e3b67204
Allan Alves <allandoamaral@gmail.com>
2015-06-14 15:16:54 -0700

    GUI changes

--- src/mass_mail/gui.clj
+++ src/mass_mail/gui.clj
@@ -15,15 +15,47 @@
 (def file-field
   (seesaw/text :text "Choose the file with the list of email addresses" :columns 40 :editable? false))
 
+(def name-field
+  (seesaw/text :text "Name" :columns 40))
+
+(def email-field
+  (seesaw/text :text "Email" :columns 30))
+
+(def password-field
+  (seesaw/text :text "Password" :columns 20))
+
+(def login-fields
+  (let [part-one email-field part-two password-field]
+    (seesaw/top-bottom-split part-one part-two)))
+
+(def name-field
+  (seesaw/text :text "Name" :columns 20))
+
+(def subject-field
+  (seesaw/text :text "Subject" :columns 20))
+
+(def name-subject-fields
+  (let [part-one name-field part-two subject-field]
+    (seesaw/top-bottom-split part-one part-two)))
+
+(def subject-login-fields
+  (let [part-one name-subject-fields part-two login-fields]
+    (seesaw/left-right-split part-one part-two)))
+
 (def content-field
   (seesaw/text :text "Content of the email" :multi-line? true :size [150 :by 120]))
 
 (def send-button
   (seesaw/button
     :text "Send email"
-    :size [150 :by 50]))
+    :size [150 :by 50] :listen [:action (fn [e] (do (seesaw/config! name-field :text (seesaw/value content-field)))
+                                                    )]))
 
-(def two-widgets
+(def content-send-fields
+  (let [part-one content-field part-two send-button]
+    (seesaw/top-bottom-split part-one part-two)))
+
+(def search-area
   (let [message file-field search-file search-action]
     (seesaw/config! search-action :listen [:action (fn [e] (if-let [f (choose-file)]
                                                            (do
@@ -33,12 +65,12 @@
     (seesaw/left-right-split message search-file)
     ))
 
-(def three-widgets
-  (let [part-one two-widgets part-two content-field]
+(def two-widgets
+  (let [part-one search-area part-two subject-login-fields]
     (seesaw/top-bottom-split part-one part-two)))
 
-(defn four-widgets []
-  (let [part-one three-widgets part-two send-button]
+(defn three-widgets []
+  (let [part-one two-widgets part-two content-send-fields]
     (seesaw/top-bottom-split part-one part-two)))
 
 (defn display
@@ -48,6 +80,6 @@
       (seesaw/config! :content content) (seesaw/pack!) (seesaw/show!))))
 
 (defn -main [& args]
-  (display (four-widgets))
+  (display (three-widgets))
   )
 
