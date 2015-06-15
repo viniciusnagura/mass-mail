05193d25584c905efa2dc3d6199be7d5828d1729
Alexis Heijmeijer <aheijmeijer@uol.com.br>
2015-06-15 16:31:01 -0700

    Interface updated using panels and mig layout.

--- src/mass_mail/gui.clj
+++ src/mass_mail/gui.clj
@@ -5,27 +5,26 @@
   (:require [seesaw.core :as seesaw])
   (require [mass-mail.core :refer [send-email]]))
 
+(use 'seesaw.mig)
 (use 'seesaw.core)
 (use 'seesaw.chooser)
 
+(def file-field
+  (seesaw/text :text "Choose the file" :columns 30 :editable? false))
+
 (def search-action
   (seesaw/button
-    :text "Search file..."
-    :size [150 :by 50]))
-
-(def file-field
-  (seesaw/text :text "Choose the file with the list of email addresses" :columns 40 :editable? false))
+    :text "Search file"
+    :size [80 :by 25] :listen [:action (fn [e] (if-let [f (choose-file)]
+                                                  (do
+                                                    (seesaw/config! file-field :text (str f))
+                                                    )))]))
 
 (def email-field
   (seesaw/text :text "Email" :columns 30))
 
 (def password-field
-  (seesaw/password :text "Password" :columns 20))
-
-(def login-fields
-  (let [part-one email-field
-        part-two password-field]
-    (seesaw/top-bottom-split part-one part-two)))
+  (seesaw/password :text "password" :echo-char \* :columns 20))
 
 (def name-field
   (seesaw/text :text "Name" :columns 20))
@@ -33,58 +32,54 @@
 (def subject-field
   (seesaw/text :text "Subject" :columns 20))
 
-(def name-subject-fields
-  (let [part-one name-field
-        part-two subject-field]
-    (seesaw/top-bottom-split part-one part-two)))
-
-(def subject-login-fields
-  (let [part-one name-subject-fields
-        part-two login-fields]
-    (seesaw/left-right-split part-one part-two)))
-
 (def content-field
-  (seesaw/text :text "Content of the email" :multi-line? true :size [150 :by 120]))
+  (seesaw/text :text "Content of the email" :multi-line? true :columns 60 :rows 10))
 
 (def send-button
   (seesaw/button
     :text "Send email"
-    :size [150 :by 50] :listen [:action (fn [e] (do (send-email (seesaw/value file-field)(seesaw/value email-field)
+    :size [150 :by 50] :listen [:action (fn [e] (do (send-email (seesaw/value file-field) (seesaw/value email-field)
                                                                 (seesaw/value password-field)
                                                                 (seesaw/value subject-field) (seesaw/value content-field)))
-                                                    )]))
-
-(def content-send-fields
-  (let [part-one content-field
-        part-two send-button]
-    (seesaw/top-bottom-split part-one part-two)))
-
-(def search-area
-  (let [message file-field
-        search-file search-action]
-    (seesaw/config! search-action :listen [:action (fn [e] (if-let [f (choose-file)]
-                                                           (do
-                                                             (seesaw/config! file-field :text (str f))
-                                                             )))])
-    (seesaw/left-right-split message search-file)
-    ))
-
-(def two-widgets
-  (let [part-one search-area
-        part-two subject-login-fields]
-    (seesaw/top-bottom-split part-one part-two)))
-
-(defn three-widgets []
-  (let [part-one two-widgets
-        part-two content-send-fields]
-    (seesaw/top-bottom-split part-one part-two)))
+                                          )]))
 
 (defn display
   [content]
   (let [window (seesaw/frame :title "Mass Email" :on-close :exit)]
     (-> window
-      (seesaw/config! :content content) (seesaw/pack!) (seesaw/show!))))
+        (seesaw/config! :content content) (seesaw/pack!) (seesaw/show!))))
+
+(def grid-file (seesaw/grid-panel
+                    :border "Choose a file"
+                    :columns 2
+                    :items [file-field search-action]))
+
+(def grid-credentials (seesaw/grid-panel
+                 :border "Credentials"
+                 :columns 2
+                 :items [email-field password-field]))
+
+(def grid-header (seesaw/grid-panel
+                 :border "Email header"
+                 :columns 2
+                 :items [name-field subject-field]))
+
+(def grid-body (seesaw/grid-panel
+                 :border "E-mail"
+                 :columns 1
+                 :items [content-field]))
+
+(def panel
+  (mig-panel
+    :items [[grid-file "wrap"]
+            [grid-credentials "wrap"]
+            [grid-header "wrap"]
+            [grid-body "wrap"]
+            [send-button "center"]
+            ]
+  )
+)
 
 (defn -main [& args]
-  (display (three-widgets))
+  (display panel)
   )
