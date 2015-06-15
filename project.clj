b6f07054c600a15b2e8749a68e7142241210f4db
Alexis Heijmeijer <aheijmeijer@uol.com.br>
2015-06-15 13:37:40 -0700

    Sending emails through map generated with the CSV file.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [com.draines/postal "1.11.3"]
-                 [seesaw "1.4.4"]]
+                 [seesaw "1.4.4"]
+                 [semantic-csv "0.1.0"]]
   :main ^:skip-aot mass-mail.gui
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mass_mail/core.clj
+++ src/mass_mail/core.clj
@@ -1,9 +1,45 @@
 (ns mass-mail.core
   (:gen-class)
   (require [clojure.tools.cli :refer [cli]]
-           [postal.core :refer [send-message]]))
+           [postal.core :refer [send-message]]
+           [semantic-csv.core :refer :all]
+           [clojure-csv.core :as csv]
+           [clojure.java.io :as io]))
+
+(defn read-file
+  [file]
+  (let [output (with-open [in-file (io/reader file)]
+               (->>
+                 (csv/parse-csv in-file)
+                 mappify
+                 remove-comments
+                 doall))]
+    output
+    )
+)
 
 (defn send-email
+  "Set all the informations that were given through command line or gui"
+  ([opts]
+   (let [{file :file email :email password :password subject :subject body :body}
+         opts]
+     (send-email file email password subject body)))
+
+  ([file email password subject body]
+   (let [dest (read-file file)
+         conn {:host "smtp.gmail.com"
+               :ssl true
+               :user email
+               :pass password}]
+
+     (mapv #(send-message conn {:from email
+                                :to (get % :email)
+                                :subject subject
+                                :body body}) dest)
+     ))
+  )
+
+(defn send-email-original
   "Set all the informations that were given through command line"
   ([opts]
     (let [{file :file name :name email :email password :password subject :subject body :body}
@@ -32,14 +68,12 @@
   (let [[opts args banner]
         (cli args
              ["-f" "--file" "REQUIRED: file containing the list of email addresses"]
-             ["-n" "--name" "NOT REQUIRED: name" :default ""]
              ["-e" "--email" "REQUIRED: email address where the message will be sent from"]
              ["-p" "--password" "REQUIRED: password for email account"]
              ["-s" "--subject" "NOT REQUIRED: subject of the message" :default ""]
              ["-b" "--body" "NOT REQUIRED: body of the message" :default ""])]
     (if (and
           (:file opts)
-          (:name opts)
           (:email opts)
           (:password opts)
           (:subject opts)
