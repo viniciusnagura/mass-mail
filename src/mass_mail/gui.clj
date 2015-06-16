1f84556477db30bb568bea33034f8f8b5e00b55d
Alexis Heijmeijer <aheijmeijer@uol.com.br>
2015-06-15 17:41:30 -0700

    Interface updated.

--- src/mass_mail/gui.clj
+++ src/mass_mail/gui.clj
@@ -15,34 +15,34 @@
   )
 
 (def file-field
-  (seesaw/text :text "Choose the file" :columns 30 :editable? false))
+  (seesaw/text :text "/" :columns 30 :editable? false))
 
 (def search-action
   (seesaw/button
-    :text "Search file"
+    :text "Browse file"
     :size [80 :by 25] :listen [:action (fn [e] (if-let [f (choose-file)]
                                                   (do
                                                     (seesaw/config! file-field :text (str f))
                                                     )))]))
 
 (def email-field
-  (seesaw/text :text "Email" :columns 30 :listen [:focus-gained (fn [e] (seesaw/text! email-field ""))]))
+  (seesaw/text :columns 30))
 
 (def password-field
-  (seesaw/password :text "password" :echo-char \* :columns 20 :listen [:focus-gained (fn [e] (seesaw/text! password-field ""))]))
+  (seesaw/password :echo-char \* :columns 20))
 
 (def name-field
-  (seesaw/text :text "Name" :columns 20 :listen [:focus-gained (fn [e] (seesaw/text! name-field ""))]))
+  (seesaw/text :columns 20))
 
 (def subject-field
-  (seesaw/text :text "Subject" :columns 20 :listen [:focus-gained (fn [e] (seesaw/text! subject-field ""))]))
+  (seesaw/text :columns 20))
 
 (def content-field
-  (seesaw/text :text "Content of the email" :multi-line? true :columns 60 :rows 10 :listen [:focus-gained (fn [e] (seesaw/text! content-field ""))]))
+  (seesaw/text :multi-line? true :columns 60 :rows 10))
 
 (def send-button
   (seesaw/button
-    :text "Send email"
+    :text "Send"
     :size [150 :by 50] :listen [:action (fn [e] (do (send-email (seesaw/value file-field) (seesaw/value email-field)
                                                                 (seesaw/value password-field)
                                                                 (seesaw/value subject-field) (seesaw/value content-field)))
